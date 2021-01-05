8c22b64a52e832db91866a531e9a968d673ea5f7
Ivan Fyodor <aamerson198@gmail.com>
2021-01-05 15:48:35 -0500

    added server and fixed got contoller set up.

--- Develop/controller/routes.js
+++ Develop/controller/routes.js
@@ -1,16 +1,63 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../model");
+const Workout = require("../models");
 
-router.get("/api/workouts", function(req, res) {
-    
+router.get("/api/workouts", (req, res) => {
+  Workout.find().then((dbWorkout) => {
+    res.json(dbWorkout);
   });
+});
 
-  router.post("/api/workouts", function(req, res) {
-    Workout.create (req.body)
-    .then(dbWorkout => 
-        res.json(dbWorkout)) 
-        .catch(err => {
-            res.json(err);
-        });
+
+// router.get("/api/donuts", (req, res) => {
+//   db.Donut.find().then((foundDonuts) => {
+//     res.json(foundDonuts);
+//   });
+// });
+router.post("/api/workouts", function (req, res) {
+  Workout.create(req.body)
+    .then(dbWorkout =>
+      res.json(dbWorkout))
+    .catch(err => {
+      res.json(err);
     });
+});
+
+// router.get("/api/config", (req, res) => {
+//   res.json({
+//     success: true,
+//   });
+// });
+
+// router.get("/api/donuts", (req, res) => {
+//   db.Donut.find().then((foundDonuts) => {
+//     res.json(foundDonuts);
+//   });
+// });
+
+// router.get("/api/donuts/:id", (req, res) => {
+//   db.Donut.findById(req.params.id).then((foundDonut) => {
+//     res.json(foundDonut);
+//   });
+// });
+
+// router.post("/api/donuts", (req, res) => {
+//   db.Donut.create(req.body).then((newDonut) => {
+//     res.json(newDonut);
+//   });
+// });
+
+// router.put("/api/donuts/:id", (req, res) => {
+//   db.Donut.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+//     (updatedDonut) => {
+//       res.json(updatedDonut);
+//     }
+//   );
+// });
+
+// router.delete("/api/donuts/:id", (req, res) => {
+//   db.Donut.findByIdAndDelete(req.params.id).then((result) => {
+//     res.json(result);
+//   });
+// });
+module.exports = router
---
+++ Develop/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+// 2. Create an instance of Express
+const app = express();
+// 3. Set the PORT
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+// 5. Add middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
+
+// routes
+
+app.use(require("./controller/routes.js"));
+
+// 4. Listen on the PORT.
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
